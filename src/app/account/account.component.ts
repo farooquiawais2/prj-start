bb4a1dc9f7eb24ff0f5dcbcc039bc43b24704ebe
Awais Farooqui <farooquiawais2@gmail.com>
2021-05-03 07:15:12 +0500

    5. Creating a Data Service

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -10,14 +10,12 @@ import { LoggingService } from '../logging.service';
 export class AccountComponent {
   @Input() account: {name: string, status: string};
   @Input() id: number;
-  @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
 
 
   constructor( private loggingService: LoggingService ) {
   }
 
   onSetTo(status: string) {
-    this.statusChanged.emit({id: this.id, newStatus: status});
     
     this.loggingService.logStatusChanged( status );
   }
